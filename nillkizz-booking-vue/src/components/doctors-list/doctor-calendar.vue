812820a20550a8405b92973eb74f1b9443125300
nillkizz <kotovvsan@yandex.ru>
2021-10-02 20:23:51 +0300

    Added heroicons vue components

--- nillkizz-booking-vue/src/components/doctors-list/doctor-calendar.vue
+++ nillkizz-booking-vue/src/components/doctors-list/doctor-calendar.vue
@@ -18,5 +18,4 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-</style>
+<style lang="sass" scoped></style>
--- nillkizz-booking-vue/src/components/ui/select.vue
+++ nillkizz-booking-vue/src/components/ui/select.vue
@@ -2,11 +2,16 @@
 .select.field(:class="{ empty: modelValue == '' }")
   select(v-model="value")
     slot
+  chevron-down-icon.chevron-down
   label {{ name }}
 </template>
 
 <script>
+import { ChevronDownIcon } from "@heroicons/vue/solid";
 export default {
+  components: {
+    ChevronDownIcon,
+  },
   props: {
     modelValue: String,
     name: String,
@@ -27,8 +32,11 @@ export default {
 <style scoped lang="sass">
 .field
   @apply border-gray-500 border relative
+  .chevron-down
+    top: calc(50% - .5rem)
+    @apply absolute right-1 text-gray-700 w-4 pointer-events-none
   select
-    @apply p-1 pt-4 w-full bg-white opacity-0 duration-300
+    @apply p-1 pt-4 w-full bg-white opacity-0 duration-300 appearance-none
   label
     top: .5rem
     @apply text-gray-400 absolute pointer-events-none duration-200 text-base left-2
